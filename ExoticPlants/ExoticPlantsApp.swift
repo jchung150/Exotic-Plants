90964d396cf2eea757b48433e4efeec78c02d33b
jchung150 <cloudfan150@gmail.com>
2024-11-03 10:45:09 -0800

    Initial Commit

---
+++ ExoticPlants/ExoticPlantsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExoticPlantsApp.swift
+//  ExoticPlants
+//
+//  Created by Juan Chung on 2024-11-03.
+//
+
+import SwiftUI
+
+@main
+struct ExoticPlantsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
